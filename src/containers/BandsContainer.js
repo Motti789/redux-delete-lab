98ecd2ef31d48e124759ce4c493767f554e0dc6c
Motti789 <mchillywilly@gmail.com>
2022-05-26 10:19:59 -0400

    commit tree 83073cb9a2c1e4b891a614cf2fa05b4c767652a6

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -7,18 +7,17 @@ class BandsContainer extends Component {
     return(
       <ul>
         <BandInput addBand={this.props.addBand}/>
-
+        <Bands bands={this.props.bands} deleteBand={this.props.deleteBand}/>
       </ul>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {bands: state.bands}
-}
+const mapStateToProps = ({ bands }) => ({ bands })
 
-const mapDispatchToProps = (dispatch) => {
-  return {addBand: name => dispatch({type: "ADD_BAND", name: name})}
-}
+const mapDispatchToProps = dispatch => ({
+  addBand: name => dispatch({ type: "ADD_BAND", name }),
+  deleteBand: id => dispatch({type: "DELETE_BAND", id})
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer); 
